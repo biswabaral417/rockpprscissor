a095c1eb26c8225f7f3c8dc7e7530e92f63fb00c
biswa baral <131336755+biswabaral417@users.noreply.github.com>
2023-05-05 11:46:48 +0545

    added ui merge navayeya tanab

--- index.js
+++ index.js
@@ -1,89 +1,81 @@
-let computer_choice = "";
-let user_choice = "";
+let game = document.querySelectorAll('.ubtn');
+let allbtns = document.querySelectorAll('.btns')
 
+let u_scoret = document.querySelector('#usr');
+let c_scoret = document.querySelector('#comp');
 
-let rbtn=document.querySelector('#rbtn');
-let pprbtn=document.querySelector('#pprbtn');
-let scbtn=document.querySelector('#scbtn');
-let crbtn=document.querySelector('#crbtn');
-let cpprbtn=document.querySelector('#cpprbtn');
-let cscbtn=document.querySelector('#cscbtn');
-let resulttxt=document.querySelector('#resulttxt');
-
-
-function a() {//this function what is chosen by computer and give border to it
+function comc() {//this function what is chosen by computer and give border to it
     rndm = Math.floor(Math.random() * 100);
     if (rndm >= 66) {
-        crbtn.style.border="10px solid red"
-        cpprbtn.style.border="none"
-        cscbtn.style.border="none"
         return "rock";
     }
-
+    
     else if (rndm >= 33) {
-        cscbtn.style.border="10px solid red"
-        crbtn.style.border="none"
-        cpprbtn.style.border="none"
         return "scissor";
     }
-
+    
     else {//this is the rest 0-33 and 100 assined to paper
-        cpprbtn.style.border="10px solid red"
-        cscbtn.style.border="none"
-        crbtn.style.border="none"
         return "paper";
     }
 }
 
-
-function compare() {//this function compares and determine wheather the user won or lost
+function compare(computer_choice,user_choice) {//this function compares and determine wheather the user won or lost
     if (user_choice == computer_choice) {
-        resulttxt.innerText="draw";
+        return "draw";
     }
     else if (user_choice == "rock" && computer_choice == "scissor") {
-        resulttxt.innerText='u win';
+        return 'win';
     }
     else if (user_choice == "paper" && computer_choice == "rock") {
-        resulttxt.innerText='u win';
+        return 'win';
     }
-    else if(user_choice == "scissor" && computer_choice == "paper"){
-        resulttxt.innerText='u win';
+    else if (user_choice == "scissor" && computer_choice == "paper") {
+        return 'win';
     }
     else {
-        resulttxt.innerText='u lose';
+        return 'lose';
+    }
+    
+}
+
+let u_score = 0;
+let c_score = 0;
+function scores(result) {
+    console.log(result);
+    if (result == "win") {
+        u_score+=1;
+        u_scoret.textContent=`${u_score}`
+        
+    }
+    else if (result == "lose") {
+        c_score+=1;
+        c_scoret.textContent=`${c_score}`
+    }
+    else {
+        u_score+=1;
+        c_score+=1;
+        u_scoret.textContent=`${u_score}`
+        c_scoret.textContent=`${c_score}`
+        
+
     }
 
 }
+function borders(uc, cc) {
+    allbtns.forEach(element => {
+        element.style.border = "";
+    });
+    document.querySelector(`#${uc}`).style.border = "10px solid green";
+    document.querySelector(`#c${cc}`).style.border = "10px solid red";
 
-//below function are called when a button is pressed
-
-function rock() {
-    user_choice = "rock";
-    rbtn.style.border="10px solid green"
-    pprbtn.style.border="none"
-    scbtn.style.border="none"
-    computer_choice = a();
-    compare();
 }
+game.forEach(element => {
+    element.addEventListener("click", (e) => {
+        let cc = comc();
+        let uc = element.getAttribute("id");
+        borders(uc, cc);
+        let result = compare(cc,uc);
+        scores(result)
+    });
+});
 
-
-
-function scissor() {
-    user_choice = "scissor";
-    scbtn.style.border="10px solid green"
-    rbtn.style.border="none"
-    pprbtn.style.border="none"
-    computer_choice = a();
-    compare();
-}
-
-
-
-function paper() {
-    user_choice = "paper";
-    pprbtn.style.border="10px solid green"
-    scbtn.style.border="none"
-    rbtn.style.border="none"
-    computer_choice = a();
-    compare();
-}
